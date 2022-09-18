8703021b99725cd6664e5d21a23d8b01a9cdcb2c
Mohamed Fayez <104222476+KingMoroAcademy@users.noreply.github.com>
2022-09-18 10:16:28 +0200

    Update main.js
    1 - Remove the testing alert
    2 - Open all external links in new tab

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-alert("Hello");
+/* Start open all external links in new tab */
+window.addEventListener("DOMContentLoaded", e =>
+{
+  document.querySelectorAll('a[href]').forEach(a =>
+  {
+    if (location.hostname == new URL(a.href).hostname)
+      return;
+
+    a.target = "_blank";
+    a.rel = "noreferrer nofollow noopener";
+  });
+});
+/* End open all external links in new tab */
